03c04c5c380b5fc7de3f9e54b1e79d9319c61a83
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-02-22 18:48:21 +0330

    Change in docs

--- docs/structure.py
+++ docs/structure.py
@@ -1,66 +1,26 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 General documentation architecture:
 Home
-Index
 - Getting started
-    Getting started with the sequential model
-    Getting started with the functional api
+    Simple Example
     FAQ
-- Models
-    About Keras models
-        explain when one should use Sequential or functional API
-        explain compilation step
-        explain weight saving, weight loading
-        explain serialization, deserialization
-    Sequential
-    Model (functional API)
-- Layers
-    About Keras layers
-        explain common layer functions: get_weights, set_weights, get_config
-        explain input_shape
-        explain usage on non-Keras tensors
-    Core Layers
-    Convolutional Layers
-    Pooling Layers
-    Locally-connected Layers
-    Recurrent Layers
-    Embedding Layers
-    Merge Layers
-    Advanced Activations Layers
-    Normalization Layers
-    Noise Layers
-    Layer Wrappers
-    Writing your own Keras layers
-- Preprocessing
-    Sequence Preprocessing
-    Text Preprocessing
-    Image Preprocessing
-Losses
-Metrics
-Optimizers
-Activations
-Callbacks
-Datasets
-Applications
-Backend
-Initializers
-Regularizers
-Constraints
-Visualization
-Scikit-learn API
-Utils
-Contributing
-'''
+- API
+    - Models
+        About scGen models
+        VAE
+    - read_load
+    - plotting
+"""
 
 from scgen import models
-from scgen import read_load
 from scgen import plotting
+from scgen import read_load
 from scgen.models import util
 
 PAGES = [
     {
-        'page': 'models/vae.md',
+        'page': 'api/models/vae.md',
         'classes': [models.VAEArith],
         'methods': [models.VAEArith.linear_interpolation,
                     models.VAEArith.predict,
@@ -70,16 +30,15 @@ PAGES = [
                     models.VAEArith.train]
     },
     {
-        'page': 'read_load.md',
+        'page': 'api/read_load.md',
         'all_module_functions': [read_load],
     },
     {
-        'page': 'models/utils.md',
+        'page': 'api/models/utils.md',
         'all_module_functions': [util],
     },
     {
-        'page': 'plotting.md',
+        'page': 'api/plotting.md',
         'all_module_functions': [plotting],
     },
 ]
-
