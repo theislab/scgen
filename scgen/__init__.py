b87bad07cb326263dd43931462011d2b190cb5dd
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-02-22 18:55:44 +0330

    Add sketch, change in mkdocs, add `six` to requirements.txt

--- scgen/__init__.py
+++ scgen/__init__.py
@@ -7,10 +7,12 @@ from . import plotting
 
 __author__ = ', '.join([
     'Mohammad  Lotfollahi',
+    'Mohsen Naghipourfar'
 ])
 
 __email__ = ', '.join([
     'Mohammad.lotfollahi@helmholtz-muenchen.de',
+    'mohsen.naghipourfar@gmail.com'
 ])
 
 from get_version import get_version
