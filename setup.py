89e968c07d0c0784001e276ae84f91db9e9f36c1
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-02-22 19:03:16 +0330

    fix setup

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.readlines()[1]
 
 setup(name='scgen',
-      version="0.0.0",
+      version="0.0.1",
       description='scGen is a generative model to predict single-cell perturbation response accross cell types, studies and species.',
       long_description=long_description,
       long_description_content_type="text/markdown",
-      url='https://github.com/M0hammadL/scgen',
+      url='https://github.com/theislab/scgen',
       author='Mohammad Lotfollahi, Mohsen Naghipourfar',
       author_email='mohammad.lotfollahi@helmholtz-muenchen.de, mohsen.naghipourfar@gmail.com',
       license='MIT',
