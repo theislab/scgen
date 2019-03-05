b00d80019ffb54de78c47467b60cd5c2ae51a608
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-03-05 23:15:40 +0330

    fix minor bug and some changes in hyperopt

--- scgen/hyperoptim.py
+++ scgen/hyperoptim.py
@@ -1,7 +1,5 @@
 from __future__ import print_function
 
-import os
-
 import numpy as np
 import scanpy as sc
 from hyperas import optim
@@ -12,27 +10,11 @@ import scgen
 
 
 def data():
-    """
-    Data providing function:
-
-    This function is separated from create_model() so that hyperopt
-    won't reload data for each evaluation run.
-    """
     x_train = sc.read("../data/train.h5ad")
     return x_train
 
 
 def create_model(x_train):
-    """
-    Model providing function:
-
-    Create Keras model with double curly brackets dropped-in as needed.
-    Return value has to be a valid python dictionary with two customary keys:
-        - loss: Specify a numeric evaluation metric to be minimized
-        - status: Just use STATUS_OK and see hyperopt documentation if not feasible
-    The last one is optional, though recommended, namely:
-        - model: specify the model just created so that we can later use it again.
-    """
     network = scgen.MMDCVAE(x_dimension=x_train.X.shape[1], z_dimension={{choice([10, 20, 50, 75, 100])}},
                             alpha={{choice([0.1, 0.01, 0.001])}}, beta={{choice([1, 10, 100, 1000])}},
                             batch_mmd=True, kernel={{choice(["multi-scale-rbf", "rbf"])}}, train_with_fake_labels=False,
@@ -42,7 +24,7 @@ def create_model(x_train):
                            batch_size={{choice([256, 512, 768, 1024, 1280, 1536, 1792, 2048])}},
                            verbose=2,
                            shuffle=True,
-                           save=False)[0]
+                           save=False)
     best_mmd_loss = np.amax(result.history['mmd_loss'])
     print('Best validation acc of epoch:', best_mmd_loss)
     return {'loss': best_mmd_loss, 'status': STATUS_OK, 'model': network.cvae_model}
