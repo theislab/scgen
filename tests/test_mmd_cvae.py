2820805c3cbe6fd13b13f608cbc8cb2e79d5450e
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-03-06 19:50:51 +0330

    change the structure of test function

--- tests/test_mmd_cvae.py
+++ tests/test_mmd_cvae.py
@@ -17,13 +17,33 @@ if not os.getcwd().endswith("tests"):
 # os.chdir("./" + current_time)
 
 
-def test_train_whole_data_one_celltype_out(z_dim=50, alpha=0.001, beta=100, kernel="multi-scale-rbf", n_epochs=1000,
-                                           batch_size=1024, cell_type_key="cell_type", condition_key="condition"):
-    train = sc.read("../data/ch10_train_7000.h5ad", backup_url="https://goo.gl/33HtVh")
+def test_train_whole_data_one_celltype_out(data_name="pbmc",
+                                           z_dim=50,
+                                           alpha=0.001,
+                                           beta=100,
+                                           kernel="multi-scale-rbf",
+                                           n_epochs=1000,
+                                           batch_size=1024,
+                                           condition_key="condition"):
+    if data_name == "pbmc":
+        stim_key = "stimulated"
+        ctrl_key = "control"
+        cell_type_key = "cell_type"
+        train = sc.read("../data/train.h5ad")
+    elif data_name == "hpoly":
+        stim_key = "Hpoly.Day10"
+        ctrl_key = "Control"
+        cell_type_key = "cell_label"
+        train = sc.read("../data/ch10_train_7000.h5ad")
+    elif data_name == "salmonella":
+        stim_key = "Salmonella"
+        ctrl_key = "Control"
+        cell_type_key = "cell_label"
+        train = sc.read("../data/chsal_train_7000.h5ad")
     for cell_type in train.obs[cell_type_key].unique().tolist():
-        os.makedirs(f"./results/{cell_type}/", exist_ok=True)
-        os.chdir(f"./results/{cell_type}")
-        net_train_data = train[~((train.obs[cell_type_key] == cell_type) & (train.obs[condition_key] == "stimulated"))]
+        os.makedirs(f"./results/{data_name}/{cell_type}/", exist_ok=True)
+        os.chdir(f"./results/{data_name}/{cell_type}")
+        net_train_data = train[~((train.obs[cell_type_key] == cell_type) & (train.obs[condition_key] == stim_key))]
 
         network = scgen.MMDCVAE(x_dimension=net_train_data.X.shape[1], z_dimension=z_dim, alpha=alpha, beta=beta,
                                 batch_mmd=True, kernel=kernel, train_with_fake_labels=False,
@@ -82,12 +102,12 @@ def test_train_whole_data_one_celltype_out(z_dim=50, alpha=0.001, beta=100, kern
                                                           decoder_labels=true_labels, data_space='latent')
 
         cell_type_data = train[train.obs[cell_type_key] == cell_type]
-        unperturbed_data = train[((train.obs[cell_type_key] == cell_type) & (train.obs[condition_key] == "control"))]
+        unperturbed_data = train[((train.obs[cell_type_key] == cell_type) & (train.obs[condition_key] == ctrl_key))]
         true_labels = np.zeros((len(unperturbed_data), 1))
         fake_labels = np.ones((len(unperturbed_data), 1))
 
         sc.tl.rank_genes_groups(cell_type_data, groupby=condition_key, n_genes=100)
-        diff_genes = cell_type_data.uns["rank_genes_groups"]["names"]["stimulated"]
+        diff_genes = cell_type_data.uns["rank_genes_groups"]["names"][stim_key]
         # cell_type_data = cell_type_data.copy()[:, diff_genes.tolist()]
 
         pred = network.predict(data=unperturbed_data, encoder_labels=true_labels, decoder_labels=fake_labels)
@@ -96,17 +116,18 @@ def test_train_whole_data_one_celltype_out(z_dim=50, alpha=0.001, beta=100, kern
         all_adata = cell_type_data.concatenate(pred_adata)
 
         scgen.plotting.reg_mean_plot(all_adata, condition_key=condition_key,
-                                     axis_keys={"x": "pred", "y": "stimulated", "y1": "stimulated"},
+                                     axis_keys={"x": "pred", "y": stim_key, "y1": stim_key},
                                      gene_list=diff_genes,
                                      path_to_save=f"./figures/reg_mean_{z_dim}.pdf")
         scgen.plotting.reg_var_plot(all_adata, condition_key=condition_key,
-                                    axis_keys={"x": "pred", "y": "stimulated", 'y1': "stimulated"},
+                                    axis_keys={"x": "pred", "y": stim_key, 'y1': stim_key},
                                     gene_list=diff_genes,
                                     path_to_save=f"./figures/reg_var_{z_dim}.pdf")
 
         sc.pp.neighbors(all_adata)
         sc.tl.umap(all_adata)
-        sc.pl.umap(all_adata, color=condition_key, save="pred")
+        sc.pl.umap(all_adata, color=condition_key,
+                   save="pred")
 
         sc.pl.violin(all_adata, keys=diff_genes.tolist()[0], groupby=condition_key,
                      save=f"_{z_dim}_{diff_genes.tolist()[0]}")
@@ -114,29 +135,45 @@ def test_train_whole_data_one_celltype_out(z_dim=50, alpha=0.001, beta=100, kern
         os.chdir("../../../")
 
 
-def reconstruct_whole_data(cell_type_key="cell_type", condition_key="condition"):
-    train = sc.read("../data/ch10_train_7000.h5ad", backup_url="https://goo.gl/33HtVh")
+def reconstruct_whole_data(data_name="pbmc", cell_type_key="cell_type", condition_key="condition"):
+    if data_name == "pbmc":
+        stim_key = "stimulated"
+        ctrl_key = "control"
+        cell_type_key = "cell_type"
+        train = sc.read("../data/train.h5ad")
+    elif data_name == "hpoly":
+        stim_key = "Hpoly.Day10"
+        ctrl_key = "Control"
+        cell_type_key = "cell_label"
+        train = sc.read("../data/ch10_train_7000.h5ad")
+    elif data_name == "salmonella":
+        stim_key = "Salmonella"
+        ctrl_key = "Control"
+        cell_type_key = "cell_label"
+        train = sc.read("../data/chsal_train_7000.h5ad")
     all_data = anndata.AnnData()
     for cell_type in train.obs[cell_type_key].unique().tolist():
-        os.chdir(f"./results/new/{cell_type}")
-        net_train_data = train[~((train.obs[cell_type_key] == cell_type) & (train.obs[condition_key] == "stimulated"))]
+        os.chdir(f"./results/{data_name}/{cell_type}")
+        net_train_data = train[~((train.obs[cell_type_key] == cell_type) & (train.obs[condition_key] == stim_key))]
         network = scgen.MMDCVAE(x_dimension=net_train_data.X.shape[1], z_dimension=50, alpha=0.001, beta=100,
                                 batch_mmd=True, kernel="multi-scale-rbf", train_with_fake_labels=False,
                                 model_path=f"./")
         network.restore_model()
 
         cell_type_data = train[train.obs[cell_type_key] == cell_type]
-        cell_type_ctrl_data = train[(train.obs[cell_type_key] == cell_type_data) & (train.obs[condition_key] == "control")]
-        unperturbed_data = train[((train.obs[cell_type_key] == cell_type) & (train.obs[condition_key] == "control"))]
+        cell_type_ctrl_data = train[
+            (train.obs[cell_type_key] == cell_type_data) & (train.obs[condition_key] == ctrl_key)]
+        unperturbed_data = train[((train.obs[cell_type_key] == cell_type) & (train.obs[condition_key] == ctrl_key))]
         true_labels = np.zeros((len(unperturbed_data), 1))
         fake_labels = np.ones((len(unperturbed_data), 1))
         pred = network.predict(data=unperturbed_data, encoder_labels=true_labels, decoder_labels=fake_labels)
         ctrl_reconstructed = network.predict(data=cell_type_ctrl_data,
-                                                       encoder_labels=np.zeros(shape=(len(cell_type_ctrl_data), 1)),
-                                                       decoder_labels=np.zeros(shape=(len(cell_type_ctrl_data), 1)))
+                                             encoder_labels=np.zeros(shape=(len(cell_type_ctrl_data), 1)),
+                                             decoder_labels=np.zeros(shape=(len(cell_type_ctrl_data), 1)))
         pred_adata = anndata.AnnData(pred, obs={condition_key: [f"{cell_type}_pred_stim"] * len(pred)},
                                      var={"var_names": cell_type_data.var_names})
-        ctrl_adata = anndata.AnnData(ctrl_reconstructed, obs={condition_key: [f"{cell_type}_pred_stim"] * len(ctrl_reconstructed)},
+        ctrl_adata = anndata.AnnData(ctrl_reconstructed,
+                                     obs={condition_key: [f"{cell_type}_pred_stim"] * len(ctrl_reconstructed)},
                                      var={"var_names": cell_type_data.var_names})
         all_data = all_data.concatenate(cell_type_data)
         all_data = all_data.concatenate(pred_adata)
@@ -145,12 +182,12 @@ def reconstruct_whole_data(cell_type_key="cell_type", condition_key="condition")
 
 
 if __name__ == '__main__':
-    test_train_whole_data_one_celltype_out(z_dim=100,
+    test_train_whole_data_one_celltype_out(data_name="hpoly",
+                                           z_dim=100,
                                            alpha=0.01,
                                            beta=1,
                                            kernel="multi-scale-rbf",
-                                           n_epochs=1500,
+                                           n_epochs=0,
                                            batch_size=768,
-                                           cell_type_key="cell_label",
                                            condition_key="condition")
     # reconstruct_whole_data()
