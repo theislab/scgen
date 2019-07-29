02a3537095a0f85f2c5436189890c5a355b97333
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-07-29 16:25:15 +0200

    remove CVAE

--- scgen/models/__init__.py
+++ scgen/models/__init__.py
@@ -1,4 +1,3 @@
 from ._vae_keras import VAEArithKeras
 from ._vae import VAEArith
 from .util import batch_removal, label_encoder, visualize_trained_network_results, data_remover, balancer
-from ._cvae import CVAE
