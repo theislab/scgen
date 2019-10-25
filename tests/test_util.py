286075d514efc3f9f1d0b63d9980afb931322673
M0hammadL <33202701+M0hammadL@users.noreply.github.com>
2019-10-25 14:57:24 +0200

    Update URL

--- tests/test_util.py
+++ tests/test_util.py
@@ -3,7 +3,7 @@ import scanpy as sc
 
 
 def test_batch_removal():
-    train = sc.read("./data/pancreas.h5ad", backup_url="https://goo.gl/V29FNk")
+    train = sc.read("./data/pancreas.h5ad", backup_url="https://www.dropbox.com/s/qj1jlm9w10wmt0u/pancreas.h5ad?dl=1")
     train.obs["cell_type"] = train.obs["celltype"].tolist()
     network = scgen.VAEArith(x_dimension=train.shape[1], model_path="./models/batch")
     network.train(train_data=train, n_epochs=1, verbose=1)
